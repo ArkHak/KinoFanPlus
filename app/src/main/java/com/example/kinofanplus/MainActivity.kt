70fef99adb5967d4aeeb1f356786c60867e83766
mys_ya_ <7nacu7@gmail.com>
2021-11-02 21:25:56 +0300

    version 0.1.1 +добавлена BottomNavigation+NavigationGraph(работают переключения) +подключено viewBinding

--- app/src/main/java/com/example/kinofanplus/MainActivity.kt
+++ app/src/main/java/com/example/kinofanplus/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.kinofanplus
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.kinofanplus.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val buttonNavigationPanel: BottomNavigationView = binding.buttonNavigationPanel
+        val navigationController = findNavController(R.id.navigation_fragment_container)
+        buttonNavigationPanel.setupWithNavController(navigationController)
+
     }
 }
