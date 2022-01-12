5f16424b298210abe00afa7b938a9def963ca47b
7nacu <7nacu7@gmail.com>
2022-01-12 22:52:56 +0300

    version 0.11.7 ***Lesson11***
    + подключен FirebaseCloudMessage, работают push
    
    * add разбивку по каналам

---
+++ app/src/main/java/com/example/kinofanplus/model/MyFirebaseMessagingService.kt
@@ -0,0 +1,89 @@
+package com.example.kinofanplus.model
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import com.example.kinofanplus.R
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+const val TAG = "MyFirebase"
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.d(TAG, "onCreate")
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        Log.d(TAG, "onMessageReceived ${remoteMessage.data}")
+
+        val remoteMessageData = remoteMessage.data
+        if (remoteMessageData.isNotEmpty()) {
+            handleDataMessage(remoteMessageData.toMap())
+        } else {
+            handleDataMessage(
+                mapOf(
+                    "title" to "Myyyy title",
+                    "message" to "Myyy message"
+                )
+            )
+        }
+    }
+
+    private fun handleDataMessage(remoteMessageData: Map<String, String>) {
+        val title = remoteMessageData[PUSH_KEY_TITLE]
+        val message = remoteMessageData[PUSH_KEY_MESSAGE]
+
+        if (!title.isNullOrBlank() && !message.isNullOrBlank()) {
+            showNotification(title, message)
+        }
+
+    }
+
+    private fun showNotification(title: String, message: String) {
+        val notification = NotificationCompat.Builder(applicationContext, CHANNEL_ID)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setSmallIcon(R.drawable.notification_ic)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        //Создание канала Notification
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel(notificationManager)
+        }
+
+        notificationManager.notify(NOTIFICATION_ID, notification.build())
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(notificationManager: NotificationManager) {
+        val name = "First Channel"
+        val descriptionText = "First Channel description"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+            description = descriptionText
+        }
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+        Log.d(TAG, "onNewToken = $token")
+    }
+
+    companion object {
+        private const val PUSH_KEY_TITLE = "title"
+        private const val PUSH_KEY_MESSAGE = "message"
+        private const val CHANNEL_ID = "channel_id"
+        private const val NOTIFICATION_ID = 37
+    }
+}
--- app/src/main/java/com/example/kinofanplus/view/App.kt
+++ app/src/main/java/com/example/kinofanplus/view/App.kt
@@ -1,15 +1,24 @@
 package com.example.kinofanplus.view
 
 import android.app.Application
+import android.util.Log
 import androidx.room.Room
 import com.example.kinofanplus.model.database.MyMovieDao
 import com.example.kinofanplus.model.database.MyMovieDataBase
+import com.google.firebase.messaging.FirebaseMessaging
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
         appInstance = this
+
+        //генерация токена
+        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Log.d("MyFirebase", "token = ${task.result}")
+            }
+        }
     }
 
     companion object {
